1d0e29056ef8671e4aba4af289d13ed49f5187dc
Apoorv Gautam <apoorvgtm@gmail.com>
2026-06-10 15:54:16 +0530

    Cors issue fixed

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://agent-mira-sable.vercel.app/"],
+    allow_origins=["http://localhost:5173", "https://agent-mira-sable.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
